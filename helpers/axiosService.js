e21310dc50eb7d4d5c9d410bb37735e3c976d8d0
Nozura <nodhzen@gmail.com>
2021-04-14 08:05:00 +0700

    edit header

--- helpers/axiosService.js
+++ helpers/axiosService.js
@@ -23,7 +23,11 @@ const AxiosService = async (url) => {
         jar: cookiejar,
         withCredentials: true,
         headers: {
-          Host: "https://komiku.id",
+          "Content-Type": "text/html",
+          Connection: "keep-alive",
+          Host: "localhost:3000",
+          "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0",
         },
       });
       if (response.status === 200) {
