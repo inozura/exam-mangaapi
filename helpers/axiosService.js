3cd1ff2628ffce9ce10e451f2cca98f5d518ae74
Nozura <nodhzen@gmail.com>
2021-04-14 08:06:22 +0700

    edit header

--- helpers/axiosService.js
+++ helpers/axiosService.js
@@ -25,7 +25,7 @@ const AxiosService = async (url) => {
         headers: {
           "Content-Type": "text/html",
           Connection: "keep-alive",
-          Host: "localhost:3000",
+          // Host: "localhost:3000",
           "User-Agent":
             "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0",
         },
